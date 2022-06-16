5bc3b7ff05315692f127c832a52d62a402f36073
Felipe Kcomt <felipe_kcomt@hotmail.com>
2022-06-16 10:33:52 -0500

    initial project configuration

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const { plugins } = require('@babel/preset-env/lib/plugins-compat-data');
+const { resolve } = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './src/index.js',
+    output : {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode : 'development',
+    resolve: {
+        extensions: ['.js','.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
+}
